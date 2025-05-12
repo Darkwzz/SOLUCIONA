6c44d90e77bb510675d25d2aef7d63e130207948
chaves nicolly <chavesnicolly052@gmail.com>
2025-05-12 09:19:58 -0300

    att_servi

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const noti_flavia = document.querySelector('.pedido_flavia')
 const noti_tiago = document.querySelector('.pedido_tiago')
 const noti_lucas = document.querySelector('.pedido_lucas')
 const noti_rafael = document.querySelector('.pedido_rafael')
+const sem_servi = document.querySelector('.aviso_servi')
 
 // document serviços //
 
@@ -20,6 +21,7 @@ const menu_perfil = document.querySelector('.menu_saldo')
 const button_perfil = document.querySelector('.button_perfil')
 
 
+
 function enviarPedido(nome){
     alert('Pedido enviado!')
     localStorage.setItem(`.pedido_${nome}`, 'true')
@@ -29,18 +31,21 @@ function aceitar(nome){
     alert('Pedido Aceito!!')
     localStorage.setItem(`.pedido_${nome}`, 'false')
     localStorage.setItem(`.servi_${nome}`, 'true')
+    localStorage.setItem('sem_servi', 'false')
     location.reload();
 }
 
 function recusar(nome){
     alert('Pedido Recusado!!')
     localStorage.setItem(`.pedido_${nome}`, 'false')
+    localStorage.setItem('sem_servi', 'false')
     location.reload();
 }
 
 function finalizar(nome){
     alert('Pedido finalizado!')
     localStorage.setItem(`.servi_${nome}`, 'false')
+    localStorage.setItem('sem_servi', 'true')
     location.reload();
 }
 
@@ -72,10 +77,16 @@ if(localStorage.getItem('.pedido_rafael') === 'true'){
     noti_rafael.style.display =  'block';
 }
 
+
 // SERVIÇOS PENDENTES //
+
+if(localStorage.getItem('sem_servi') === 'false'){
+    sem_servi.style.display = 'none';
+}
 
 if(localStorage.getItem('.servi_flavia') === 'true'){
     servi_flavia.style.display =  'block';
+    
 }
 
 if(localStorage.getItem('.servi_lucas') === 'true'){
